fb9ccd7f589d34e399f8b70c5ce63024f8779998
Irene Trigueros <48659055+Itrilor@users.noreply.github.com>
2022-11-25 22:13:28 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,12 +17,50 @@ int main(int argc, char ** argv){
   QKP sequence;
   bool estado;
 
+  //------------RANDOM------------
   cout << "Leemos " << argv[1] << "\n";
   estado = sequence.leerFicheroDatos(argv[1]);
   if(!estado){
     cerr << "No se ha podido leer correctamente\n";
     exit(-1);
   }
+
+  auto start = std::chrono::high_resolution_clock::now();
+  sequence.RandomQKP();
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double,std::micro> float_ms = end-start;
+  double tiempoRandom = float_ms.count();
+
+  cout << "La secuencia obtenida por el algoritmo Random es:\n { ";
+  for(int i = 0; i < sequence.getSolucion().size(); ++i){
+    cout << sequence.getSolucion()[i] << ",";
+  }
+  cout << "}\nEl valor alcanzado sería: " << sequence.getValorSolucion() << "\n";
+  cout << "El peso alcanzado sería: " << sequence.getPesoSolucion() << "\n";
+  cout << "Tiempo de ejecución: " << tiempoRandom << "microsegundos\n";
+
+  //------------GREEDY------------
+  cout << "Leemos " << argv[1] << "\n";
+  estado = sequence.leerFicheroDatos(argv[1]);
+  if(!estado){
+    cerr << "No se ha podido leer correctamente\n";
+    exit(-1);
+  }
+
+  start = std::chrono::high_resolution_clock::now();
+  sequence.Greedy();
+  end = std::chrono::high_resolution_clock::now();
+  float_ms = end-start;
+  double tiempoGreedy = float_ms.count();
+
+  cout << "La secuencia obtenida por el algoritmo Greedy es:\n { ";
+  for(int i = 0; i < sequence.getSolucion().size(); ++i){
+    cout << sequence.getSolucion()[i] << ",";
+  }
+  cout << "}\nEl valor alcanzado sería: " << sequence.getValorSolucion() << "\n";
+  cout << "El peso alcanzado sería: " << sequence.getPesoSolucion() << "\n";
+  cout << "Tiempo de ejecución: " << tiempoGreedy << "microsegundos\n";
+
 }
 
 void mensajeError(){
